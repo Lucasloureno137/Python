d57f39f04b858ef080adc2c0b60d61c3fed16bc7
Lucas dos Santos Lourenço <133615481+Lucasloureno137@users.noreply.github.com>
2025-05-21 20:05:18 -0300

    Update niko.py

--- RETORNO/niko.py
+++ RETORNO/niko.py
@@ -1,10 +1,1 @@
-listaNomes = []
 
-for i in range(2):
-    nomes = str(input("Insira seu nome: "))
-    listaNomes.append(nomes)
-
-if 'nicolas' in listaNomes:
-    print("Hmmmm, nicolas é nome de viadinho")
-else:
-    print(listaNomes)
